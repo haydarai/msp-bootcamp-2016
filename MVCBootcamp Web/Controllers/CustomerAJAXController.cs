60cc9acbb5871744415f7437a34be483420253a1
Haydar Ali Ismail <haydar_ai@yahoo.com>
2016-05-06 09:12:29 +0700

    add jquery ajax and create customerajax controller

---
+++ MVCBootcamp Web/Controllers/CustomerAJAXController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using NorthwindRepository.Repositories;
+
+namespace MVCBootcamp_Web.Controllers
+{
+    public class CustomerAJAXController : Controller
+    {
+
+        private CustomerRepository custRepo = new CustomerRepository();
+
+        // GET: CustomerAJAX
+        public ActionResult Index()
+        {
+            return View(custRepo.GetAllData().Take(10));
+        }
+    }
+}
