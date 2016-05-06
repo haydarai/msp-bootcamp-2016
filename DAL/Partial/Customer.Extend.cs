480a09be00cd503ac54ba28d56b1d8ee20b43053
Haydar Ali Ismail <haydar_ai@yahoo.com>
2016-05-06 08:50:09 +0700

    init commit

---
+++ DAL/Partial/Customer.Extend.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace DAL
+{
+	[MetadataType(typeof(CustomerMetadata))]
+    public partial class Customer
+    {
+    }
+
+	public class CustomerMetadata
+    {
+		[Display(Name = "Customer ID")]
+		[Required(ErrorMessage = "Customer ID need to be filled.")]
+		[StringLength(5, ErrorMessage = "Max ID characters is 5.")]
+		[Remote("CheckCustomerID", "Customers", ErrorMessage ="Customer ID already exist.")]
+		public string CustomerID { get; set; }
+
+        [Display(Name = "Company Name")]
+        [Required(ErrorMessage = "Company Name need to be filled.")]
+        [StringLength(40, ErrorMessage = "Max Company Name characters is 40.")]
+        public string CompanyName { get; set; }
+    }
+}
---
+++ MVCBootcamp Web/Controllers/StoreController.cs
@@ -0,0 +1,56 @@
+﻿using MVCBootcamp_Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBootcamp_Web.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Browse(string category)
+        {
+            return HttpUtility.HtmlEncode("hello from browse with category " + category);
+        }
+
+        public string News(int year, int month, int day)
+        {
+            return HttpUtility.HtmlEncode(year.ToString() + month.ToString() + day.ToString());
+        }
+
+        public ActionResult Home()
+        {
+            return View("~/Views/Home/Contact.cshtml");
+        }
+
+        public ActionResult DataSample()
+        {
+            ViewData["event"] = "MSP Bootcamp";
+            ViewData["place"] = "Hotel Neo";
+            ViewBag.events = "MSP Bootcamp";
+            ViewBag.place = "Hotel Neo";
+
+            var books = new List<string> { "ASP", "SQL", "Windows" };
+            ViewBag.books = books;
+
+            return View();
+        }
+
+        public ViewResult StronglyTypeView()
+        {
+
+            var listCust = new List<CustomerData>();
+            listCust.Add(new CustomerData() { CompanyName = "Native Enterprise", City = "Bandung" });
+            listCust.Add(new CustomerData() { CompanyName = "Microsoft", City = "Jakarta" });
+
+            return View(listCust);
+        }
+    }
+}
