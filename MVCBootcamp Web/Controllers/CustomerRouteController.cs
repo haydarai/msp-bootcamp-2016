685c62e2f133774ee3196e556b02f7da33e28c1e
Haydar Ali Ismail <haydar_ai@yahoo.com>
2016-05-06 11:06:12 +0700

    create customerroutecontroller

---
+++ MVCBootcamp Web/Controllers/CustomerRouteController.cs
@@ -0,0 +1,26 @@
+﻿using NorthwindRepository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBootcamp_Web.Controllers
+{
+    public class CustomerRouteController : Controller
+    {
+        private CustomerRepository custRepo = new CustomerRepository();
+
+        // GET: CustomeRoute
+        public ActionResult Index()
+        {
+            return View(custRepo.GetAllData().Take(5));
+        }
+
+        public ActionResult Search(string id)
+        {
+            var cust = custRepo.Search(id);
+            return View(cust);
+        }
+    }
+}
