2a9671841176a40b30e2386fa6e01d43430537b5
Haydar Ali Ismail <haydar_ai@yahoo.com>
2016-05-07 11:58:25 +0700

    add seed in migration file

--- MVCBootcamp Web/Migrations/Configuration.cs
+++ MVCBootcamp Web/Migrations/Configuration.cs
@@ -5,6 +5,10 @@ namespace MVCBootcamp_Web.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using DAL;
+    using Models;
     internal sealed class Configuration : DbMigrationsConfiguration<MVCBootcamp_Web.Models.ApplicationDbContext>
     {
         public Configuration()
@@ -15,6 +19,32 @@ namespace MVCBootcamp_Web.Migrations
 
         protected override void Seed(MVCBootcamp_Web.Models.ApplicationDbContext context)
         {
+            if (!context.Users.Any(u => u.UserName == "admin1")) {
+                var roleStore = new RoleStore<IdentityRole>(context);
+                var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                var user = new ApplicationUser { UserName = "admin1" };
+                userManager.Create(user, "123456");
+                roleManager.Create(new IdentityRole { Name = "Administrators" });
+                userManager.AddToRole(user.Id, "Administrators");
+            }
+
+            if (!context.Users.Any(u => u.UserName == "user1")) {
+                var roleStore = new RoleStore<IdentityRole>(context);
+                var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                var user = new ApplicationUser { UserName = "user1" };
+                userManager.Create(user, "123456");
+                roleManager.Create(new IdentityRole { Name = "Members" });
+                userManager.AddToRole(user.Id, "Members");
+            }
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
